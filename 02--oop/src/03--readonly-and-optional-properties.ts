445746ba0c8f5d760937bf8ef416c4f92a9c148c
huyphamfc <huyphamfc@gmail.com>
2023-02-15 17:08:44 +0700

    Read-only & Optional Property

---
+++ 02--oop/src/03--readonly-and-optional-properties.ts
@@ -0,0 +1,19 @@
+class Account {
+  readonly id: number;
+  owner: string;
+  balance: number;
+  nickname?: string;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+
+  deposit(amount: number) {
+    if (amount <= 0) throw new Error('Invalid amount.');
+    this.balance += amount;
+  }
+}
+
+export default Account;
