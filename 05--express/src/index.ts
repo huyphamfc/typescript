39653e094c782c5d2b11a263155dc8dfd2e7c1ec
huyphamfc <huyphamfc@gmail.com>
2023-02-17 10:27:51 +0700

    Route

--- 05--express/src/index.ts
+++ 05--express/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/api', (req, res) => {
+  res.end('Hello from the server!');
+});
+
 app.listen(8000, () => {
   console.log('Server running on port 8000.');
 });
